c6718281a2887853bde90f820bc02edfd6f16dcc
2018130 <35947698+2018130@users.noreply.github.com>
2024-05-01 10:57:22 +0900

    Update 24.05.01
    Indexed Representation방식으로 메시 생성

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -26,25 +26,48 @@ int main(void)
 		cout << "Error\n";
 	}
 
-	float vertexes[6] = {
+	glEnable(GL_CULL_FACE);
+	float vertexes[] = {
 		-0.5f, -0.5f,
-		 0.0f, 0.5f,
 		 0.5f, -0.5f,
+		 0.5f, 0.5f,
+		-0.5f, 0.5f
+	};
+
+	unsigned int indices[] =
+	{
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	unsigned int bufferID;
 	glGenBuffers(1, &bufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, bufferID);
 	glBufferData(GL_ARRAY_BUFFER,
-		6 * sizeof(float),
+		8 * sizeof(float),
 		vertexes,
 		GL_STATIC_DRAW);
+
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+		6 * sizeof(unsigned int),
+		indices,
+		GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES,
+			6,
+			GL_UNSIGNED_INT,
+			nullptr);
 
 		glfwSwapBuffers(window);
 
